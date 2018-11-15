e7ea9d0b3ce069809d9574e7081dd8f1f0c71341
Tobias Kohlbau <t.kohlbau@myopenfactory.com>
2018-11-15 13:17:50 +0100

    Fix isAdmin function
    IsAdmin could be done without enrolling own syscall by using
    `windows.Token(0)`.
    
    Fixes #29
    
    Signed-off-by: Tobias Kohlbau <t.kohlbau@myopenfactory.com>

--- cmd/update_windows.go
+++ cmd/update_windows.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"strings"
 	"time"
-	"syscall"
-	"unsafe"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
@@ -16,54 +14,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// Do the interface allocations only once for common
-// Errno values.
-const (
-	errnoERROR_IO_PENDING = 997
-)
-
-var (
-	errERROR_IO_PENDING error = syscall.Errno(errnoERROR_IO_PENDING)
-)
-
-// errnoErr returns common boxed Errno values, to prevent
-// allocations at runtime.
-func errnoErr(e syscall.Errno) error {
-	switch e {
-	case 0:
-		return nil
-	case errnoERROR_IO_PENDING:
-		return errERROR_IO_PENDING
-	}
-
-	// TODO: add more here, after collecting data on the common
-	// error values see on Windows. (perhaps when running
-	// all.bat?)
-	return e
-}
-
-var (
-	modadvapi32              = windows.NewLazySystemDLL("advapi32.dll")
-	procCheckTokenMembership = modadvapi32.NewProc("CheckTokenMembership")
-)
-
 func isAdmin() (bool, error) {
 	var sid *windows.SID
 	err := windows.AllocateAndInitializeSid(&windows.SECURITY_NT_AUTHORITY, 2, windows.SECURITY_BUILTIN_DOMAIN_RID, windows.DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0, &sid)
 	if err != nil {
 		return false, err
 	}
-
-	var b int32
-	r1, _, e1 := syscall.Syscall(procCheckTokenMembership.Addr(), 3, uintptr(0), uintptr(unsafe.Pointer(sid)), uintptr(unsafe.Pointer(&b)))
-	if r1 == 0 {
-		if e1 != 0 {
-			return false, errnoErr(e1)
-		} else {
-			return false, syscall.EINVAL
-		}
-	}
-	return b != 0, nil
+	token := windows.Token(0)
+	return token.IsMember(sid)
 }
 
 func preUpdate(cmd *cobra.Command, args[]string) error {
