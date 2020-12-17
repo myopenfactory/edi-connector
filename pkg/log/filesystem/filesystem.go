dc9407012b950cb2b2b672a59f70c60942a041b0
Tobias Kohlbau <t.kohlbau@myopenfactory.com>
2020-12-17 09:43:34 +0100

    use crlf as log line endings on windows
    Fixes #45
    
    Signed-off-by: Tobias Kohlbau <t.kohlbau@myopenfactory.com>

--- pkg/log/filesystem/filesystem.go
+++ pkg/log/filesystem/filesystem.go
@@ -1,23 +1,50 @@
 package filesystem
 
 import (
+	"io"
 	"path/filepath"
+	"runtime"
 	"time"
 
 	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
-	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/text/transform"
 )
 
 var defaultFormatter = &logrus.TextFormatter{DisableColors: true}
 
+type crlfTransformer struct{}
+
+func (crlfTransformer) Transform(dst, src []byte, atEOF bool) (nDst, nSrc int, err error) {
+	for nSrc < len(src) {
+		if nDst >= len(dst) {
+			err = transform.ErrShortDst
+			break
+		}
+		c := src[nSrc]
+		if c == '\n' {
+			dst[nDst] = '\r'
+			dst[nDst+1] = c
+			nSrc++
+			nDst += 2
+		} else {
+			dst[nDst] = c
+			nSrc++
+			nDst++
+		}
+	}
+	return
+}
+
+func (crlfTransformer) Reset() {}
+
 type FilesystemHook struct {
-	path string
-	hook *lfshook.LfsHook
+	path   string
+	writer io.Writer
 }
 
 func New(path string) (*FilesystemHook, error) {
-	writer, err := rotatelogs.New(
+	logWriter, err := rotatelogs.New(
 		filepath.Join(path, "log.%Y%m%d%H%M.log"),
 		rotatelogs.WithMaxAge(24*time.Hour),
 		rotatelogs.WithRotationTime(time.Hour),
@@ -27,27 +54,31 @@ func New(path string) (*FilesystemHook, error) {
 		return nil, err
 	}
 
+	var writer io.Writer = logWriter
+
+	if runtime.GOOS == "windows" {
+		writer = transform.NewWriter(logWriter, crlfTransformer{})
+	}
+
 	return &FilesystemHook{
-		path: path,
-		hook: lfshook.NewHook(
-			lfshook.WriterMap{
-				logrus.PanicLevel: writer,
-				logrus.FatalLevel: writer,
-				logrus.ErrorLevel: writer,
-				logrus.WarnLevel:  writer,
-				logrus.InfoLevel:  writer,
-				logrus.DebugLevel: writer,
-			},
-			defaultFormatter,
-		),
+		path:   path,
+		writer: writer,
 	}, nil
 }
 
+// Fire serializes and writes the log entry to file.
 func (h *FilesystemHook) Fire(entry *logrus.Entry) error {
-	return h.hook.Fire(entry)
+	data, err := defaultFormatter.Format(entry)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = h.writer.Write(data)
+	return err
 }
 
 // Levels returns the available logging levels.
 func (h *FilesystemHook) Levels() []logrus.Level {
-	return h.hook.Levels()
+	return logrus.AllLevels
 }
